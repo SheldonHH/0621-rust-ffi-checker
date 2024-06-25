ce7760310a5ff7b4ce470b0d3c7582692e3cd795
sheldonhh <xphuangsg@gmail.com>
2024-06-25 13:31:34 +0000

    unccoment

--- examples/0625/customize-cpp-rust-use-after-free/main.cpp
+++ examples/0625/customize-cpp-rust-use-after-free/main.cpp
@@ -12,13 +12,12 @@ int main() {
     // Get string from Rust
     char* rust_string = create_string();
 
-
     // Rust frees the string
     free_string(rust_string);
 
 
 
-    // 失败❌detect
+    // 失败❌detect，复现：uncomment下面的行，comment上面的行
     // char* rust_string = combine();
     // Use after free: this will trigger an error
     std::cout << rust_string << std::endl;
